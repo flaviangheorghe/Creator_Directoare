a232983924f962f90ac97aec7b09047318bc750b
flaviangheorghe <gheorghe.flavian@gmail.com>
2016-04-04 17:25:50 +0200

    Version 1.5.5
    Added the code to the repository

---
+++ creereDirectoare.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Security.Permissions;
+
+    class creereDirectoare
+    {
+        public void copiereFisiere(string destinatar, string expeditor)
+        {
+            DirectoryInfo dest = new DirectoryInfo(destinatar);
+            string[] caleFis = Directory.GetFiles(expeditor);
+            foreach (string s in caleFis)
+            {
+                 FileInfo fisierCopiat = new FileInfo(s);
+                try
+                {
+                    fisierCopiat.CopyTo(Path.Combine(destinatar, Path.GetFileName(s)));
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+              }
+        }
+
+        public void setareDreptScriere(string caleDirector) {
+            FileIOPermission fiop = new FileIOPermission(PermissionState.None);
+            DirectoryInfo dir=new DirectoryInfo(caleDirector);
+            
+        }
+        public void copiereDirectoare(string caleDestinatar, string caleExpeditor)
+        {
+            DirectoryInfo dirExpeditor = new DirectoryInfo(caleExpeditor);
+            DirectoryInfo dirDestinatar = new DirectoryInfo(caleDestinatar);
+            this.copiereFisiere(caleDestinatar, caleExpeditor);
+            if (dirExpeditor.GetDirectories() != null)
+            {
+                foreach (DirectoryInfo d in dirExpeditor.GetDirectories())
+                {
+                    string caleDest2 = caleDestinatar + "\\" + d.Name;
+                    string caleExp2 = caleExpeditor + "\\" + d.Name;
+                    try
+                    {
+                        dirDestinatar.CreateSubdirectory(d.Name);
+                        this.copiereDirectoare(caleDest2, caleExp2);
+                    }
+                    catch (ArgumentException ae) { Console.WriteLine(ae.Message); }
+                }
+            }
+        }
+
+        public void creeereDirector(String cale) {
+            string afisare="";
+            try {
+                DirectoryInfo dir =Directory.CreateDirectory(cale);
+            }
+            catch (Exception e) { afisare = "A intervenit o eroare: " + e.Message; }
+        }
+        public void copiereDirectoare2(string Src, string Dst)
+        {
+            String[] Files;
+             if (Dst[Dst.Length - 1] != Path.DirectorySeparatorChar)
+                Dst += Path.DirectorySeparatorChar;
+            if (!Directory.Exists(Dst)) Directory.CreateDirectory(Dst);
+            Files = Directory.GetFileSystemEntries(Src);
+            foreach (string Element in Files)
+            {
+                 if (Directory.Exists(Element))
+                    copiereDirectoare2(Element, Dst + Path.GetFileName(Element));
+                else
+                    File.Copy(Element, Dst + Path.GetFileName(Element));
+                    FileInfo fisier = new FileInfo(Dst + Path.GetFileName(Element));
+                    fisier.IsReadOnly = false;
+
+            }
+        }
+   }
---
+++ lucruXML2.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Threading;
+
+class lucruXML2
+{
+    public string selectareModul(string cale, String directorSursaO) {
+        string numeModul = "";
+
+        //metoda 3
+        creereDirectoare cd = new creereDirectoare();
+        string directorSursa = directorSursaO+ @"\Syllabus_3\";
+        string directorCandidat = "";
+        string directorLocatie="";
+        string directorModulDest = "";
+        string directorModul = "";
+        string numeTest = "Test ";
+        string numeC = "";
+        string prenumeC = "";
+        string numeCand = "";
+        string modul1 ="";
+        string modul2 = "";
+        string modul3 = "";
+        string modul4 = "";
+        string modul5 = "";
+        string modul6 = "";
+        string modul7 = "";
+        XmlTextReader cititor = null; 
+        StringBuilder stXml = new StringBuilder();
+        int poz = cale.LastIndexOf(@"\") + 1;
+        directorLocatie = cale.Substring(0, poz);
+        try
+        {
+                cititor = new XmlTextReader(cale);
+                object nume = cititor.NameTable.Add("nume");
+                object prenume = cititor.NameTable.Add("prenume");
+                object Modul1 = cititor.NameTable.Add("m1");
+                object Modul2 = cititor.NameTable.Add("m2");
+                object Modul3 = cititor.NameTable.Add("m3");
+                object Modul4 = cititor.NameTable.Add("m4");
+                object Modul5 = cititor.NameTable.Add("m5");
+                object Modul6 = cititor.NameTable.Add("m6");
+                object Modul7 = cititor.NameTable.Add("m7");
+                while (cititor.Read())
+                {
+                    if (cititor.NodeType == XmlNodeType.Element)
+                    {
+                        if(cititor.Name.Equals(nume))
+                        {
+                            numeC = cititor.ReadString();
+                            numeCand = numeC;
+                        }
+                        if(cititor.Name.Equals(prenume)) {
+                            prenumeC = cititor.ReadString();
+                            numeCand += " " + prenumeC;
+                            numeModul += numeCand + "\t";
+                            directorCandidat = directorLocatie + numeCand + @"\";
+                            numeModul += directorCandidat + "\r\n";
+                            cd.creeereDirector(directorCandidat);
+                        }
+                        if(cititor.Name.Equals(Modul1) != false)
+                        {
+                            modul1 = "1." + cititor.ReadString();
+                            numeModul +=modul1 + "\r\n";
+                        }
+                        if (cititor.Name.Equals(Modul2) != false)
+                        {
+                             modul2 = "2." + cititor.ReadString();
+                            directorModul = directorSursa + @"M2\" + numeTest + modul2;
+                            directorModulDest = directorCandidat + numeTest + modul2;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul +=directorModulDest + "\r\n";
+                        }
+                        if(cititor.Name.Equals(Modul3) != false)
+                        {
+                            modul3 = "3." + cititor.ReadString() +@"\";
+                            directorModul = directorSursa + @"M3\" + numeTest + modul3;
+                            directorModulDest = directorCandidat + numeTest + modul3;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul += directorModulDest + "\r\n";
+                        }
+                        if(cititor.Name.Equals(Modul4) != false)
+                        {
+                            modul4 = "4." + cititor.ReadString() + @"\";
+                            directorModul = directorSursa + @"M4\" + numeTest + modul4;
+                            directorModulDest = directorCandidat + numeTest + modul4;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul +=directorModulDest +"\r\n";
+                        }
+                        if(cititor.Name.Equals(Modul5) != false)
+                        {
+                             modul5 = "5." + cititor.ReadString();
+                            directorModul = directorSursa + "M5\\" + numeTest + modul5;
+                            directorModulDest = directorCandidat + numeTest + modul5;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul += directorModulDest + "\r\n";
+                       }
+                        if(cititor.Name.Equals(Modul6) != false)
+                        {
+                            modul6 = "6." + cititor.ReadString();
+                            directorModul = directorSursa + @"M6\" + numeTest + modul6;
+                            directorModulDest = directorCandidat + numeTest + modul6;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul += directorModulDest + "\r\n";
+                        }
+                       if(cititor.Name.Equals(Modul7) != false)
+                       {
+                            modul7 = "7." + cititor.ReadString();
+                            directorModul = directorSursa + @"M7\" + numeTest + modul7;
+                            directorModulDest = directorCandidat + numeTest + modul7;
+                            cd.copiereDirectoare2(directorModul, directorModulDest);
+                            numeModul += directorModulDest + "\r\n";
+                        }
+                       }
+                    cititor.MoveToElement(); 
+                }
+            }
+            catch (Exception e) { numeModul = e.Message; }
+        return numeModul;
+    }
+    public string selectareNume(String cale)
+    {
+           String directorSursa = "";
+        string caleDirector = "";
+        string afisareDirCreate="";
+        int poz = cale.LastIndexOf(@"\") + 1;
+        directorSursa = cale.Substring(0, poz);
+        XElement candidati = XElement.Load(cale);
+        creereDirectoare cd = new creereDirectoare();
+         foreach (XElement nume in candidati.Elements("Student").Elements("Nume"))
+        {
+            caleDirector = "";
+            String n = nume.Value.ToString();
+            n = n.Trim();
+            caleDirector = directorSursa + n;
+            cd.creeereDirector(caleDirector);
+            afisareDirCreate += caleDirector + "\r\n";
+        }
+        return afisareDirCreate;
+    }
+}
